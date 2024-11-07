063cba6f49aa7df83c74f045afd2e05ab24f286c
olegk <kbryzhenyukpc@gmail.com>
2024-11-07 10:40:55 +0200

    Добавьте файлы проекта.

---
+++ Recursion1.4/Recursion1.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int sumRange(int a, int b) {
+    if (a > b) return 0;
+    return a + sumRange(a + 1, b);
+}
+
+int main() {
+    int a, b;
+    cout << "a и b ";
+    cin >> a >> b;
+    cout << "cумма чисел от " << a << " до " << b << " = " << sumRange(a, b) << endl;
+    return 0;
+}
